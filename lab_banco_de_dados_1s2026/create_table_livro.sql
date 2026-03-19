21dead4b0d15f65bdaecf2334eab1689c840e9f7
marcos vinicios <marcosviniciosdu@gmail.com>
2026-03-18 21:28:09 -0300

    Delete lab_banco_de_dados_1s2026/create_table_livro.sql

--- lab_banco_de_dados_1s2026/create_table_livro.sql
+++
@@ -1,9 +0,0 @@
-CREATE table livro (
-id_livro int auto_increment primary key,
-titulo varchar (255) not null,
-sinopse TEXT,
-autor varchar (128) not null,
-editora varchar (128) not null,
-categoria varchar (128) not null,
-data_publicaçao year
-);
