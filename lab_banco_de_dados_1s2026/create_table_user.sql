0ca3c02248f593244ddaa63e00644e3cff1a38fe
marcos vinicios <marcosviniciosdu@gmail.com>
2026-03-18 21:28:14 -0300

    Delete lab_banco_de_dados_1s2026/create_table_user.sql

--- lab_banco_de_dados_1s2026/create_table_user.sql
+++
@@ -1,7 +0,0 @@
-USE biblioteca_pessoal_2026;
-CREATE table Usuario(
-id_usuario int auto_increment primary key,
-username varchar (255) not null,
-email varchar (255) unique,
-data_cadastro date 
-);
